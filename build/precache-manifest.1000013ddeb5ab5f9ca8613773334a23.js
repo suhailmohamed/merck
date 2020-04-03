085cbdf950aac8f58dcfd1eef250abc5a5ba63a5
suhailmohamed <suhailmohamed333@gmail.com>
2020-04-03 19:37:13 +0530

    Setup

---
+++ build/precache-manifest.1000013ddeb5ab5f9ca8613773334a23.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = [
+  {
+    "revision": "a44f2cdb021cc7103dc1",
+    "url": "/merck/static/css/main.c02082b9.chunk.css"
+  },
+  {
+    "revision": "a44f2cdb021cc7103dc1",
+    "url": "/merck/static/js/main.a44f2cdb.chunk.js"
+  },
+  {
+    "revision": "bc66302ba7c3b42f611f",
+    "url": "/merck/static/js/runtime~main.bc66302b.js"
+  },
+  {
+    "revision": "1728995f17254d8e4c3d",
+    "url": "/merck/static/js/2.1728995f.chunk.js"
+  },
+  {
+    "revision": "c03abbceffe6c05afd51ab4e4209a773",
+    "url": "/merck/index.html"
+  }
+];
